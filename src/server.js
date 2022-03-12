aa82583850930aa8fb94c2b350aaf3124f93c3b8
Andreas Viklund <github@kritvit.se>
2022-03-12 21:41:39 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 
 const express = require('express');
-const cors = require('cors');
-const router = require('./router');
-const { PORT = 3001 } = process.env;
+const routes = require('./routes');
+const { API_PORT = 3001 } = process.env;
 
 const app = express();
 
@@ -10,13 +9,12 @@ const app = express();
 app.use(express.json());
 
 // Serve API requests from the router
-// Using CORS for when server and app are served on different ports
-app.use('/api', cors(), router);
+app.use('/api', routes);
 
 // Serve app production bundle
 app.use(express.static('dist/app'));
 
 // Listen the server
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.listen(API_PORT, () => {
+  console.log(`Server listening at http://localhost:${API_PORT}`);
 });
