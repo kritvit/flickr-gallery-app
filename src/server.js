9e61779176f1468d9f7013093d82b9ff2ca6c431
Andreas Viklund <github@kritvit.se>
2022-03-14 19:59:12 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 
 const express = require('express');
 const routes = require('./routes');
+const timeout = require('express-timeout-handler');
+
 const { API_PORT = 3001 } = process.env;
 
 const app = express();
+
+// The default timeout for all routes
+app.use(timeout.handler({
+  timeout: 10 * 1000 // Seconds
+}));
 
 // Middleware that parses json and looks at requests where the Content-Type header matches the type option.
 app.use(express.json());
