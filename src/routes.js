eacd5a0dfea7a9c031286084359f4a949d6e17b9
Andreas Viklund <github@kritvit.se>
2022-03-15 10:32:48 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -11,17 +11,16 @@ const { FLICKR_API_KEY } = process.env;
 
 router.get('/search/:query/:page/:take', async (req, res, next) => {
 
-  // https://www.npmjs.com/package/flickr-sdk
-
-  const flickr = new Flickr(FLICKR_API_KEY);
-
-  const params = {
-    "text": req.params.query,
-    "page": req.params.page,
-    "per_page": req.params.take
-  };
-
   try {
+
+    // https://www.npmjs.com/package/flickr-sdk
+    const flickr = new Flickr(FLICKR_API_KEY);
+
+    const params = {
+      "text": req.params.query,
+      "page": req.params.page,
+      "per_page": req.params.take
+    };
 
     const flickrRes = await flickr.photos.search(params);
 
